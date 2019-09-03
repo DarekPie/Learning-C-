1d5dcd53b17efc79e2dbdb2ba2f0bcb799432b04
DarekDesk <darek.piechota@gmail.com>
2019-09-03 07:30:18 +0200

    Pozostale programy 12

---
+++ 12 klasy a dynamiczny przydzial pamieci/12_4 String/12_4_String1/string1.h
@@ -0,0 +1,47 @@
+#ifndef STRING1_H_
+#define STRING1_H_
+
+#include <iostream>
+using std::ostream;
+using std::istream;
+
+class String
+{
+	private:
+		char * str;										// wkaznik ciagu
+		int len;										// dlugosc ciagu
+		static int num_strings;							//liczba obiketow klasy
+		static const int CINLIM = 80;					// limit dlugosci ciagu na wejsciu
+//		enum { CINLIM = 80 };
+//		int CINLIM = 80;					
+	public:
+		
+		//Konstruktory i pozostale metody klasy
+		String(const char * s);							// konstruktor
+		String();										// konstruktor domyslny
+		String(const String&);							// konstruktor kopiujacy
+		~String();										// destruktor
+		int length() const { return len;}
+
+		//metody przeciazajace operatory
+		String & operator=(const String &);
+		String & operator=(const char *);
+		char & operator[](int i);
+		const char & operator[](int i) const;
+
+		//Funkcje zaprzyjaznione przeciazajace operatory
+		friend bool operator<(const String &st, const String &st2);
+		friend bool operator>(const String &st1, const String &st2);
+		friend bool operator==(const String &st, const String &st2);
+		friend ostream & operator<<(ostream &os, const String &st);
+		friend istream & operator>>(istream &is, String &st); 
+
+		//metoda statyczna
+		static int HowMany();
+
+
+};
+
+
+
+#endif
