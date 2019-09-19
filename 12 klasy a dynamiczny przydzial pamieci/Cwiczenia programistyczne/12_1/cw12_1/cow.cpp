2b6338c81049cf55d2f41346489ce934f1e31d85
DarekDesk <darek.piechota@gmail.com>
2019-09-19 06:28:23 +0200

    Cwiczenie 12_1 COW dodano

--- 12 klasy a dynamiczny przydzial pamieci/Cwiczenia programistyczne/12_1/cw12_1/cow.cpp
+++ 12 klasy a dynamiczny przydzial pamieci/Cwiczenia programistyczne/12_1/cw12_1/cow.cpp
@@ -1,4 +1,5 @@
 #include <cstring>
+#include <iostream>
 using std::cin;
 using std::cout;
 using std::ostream;
@@ -7,41 +8,66 @@ using std::istream;
 
 Cow::Cow()
 {
-
-	len = 4;
-	str = new char[1];
-	str[0] = '\0';							// domyœlny ci¹g obiektów klasy
-	num_strings++;
-
-
-	name = "NAME"
-	char * hobby;
-	double weight;
-
+	name[0] = '\0';
+	int len = 2;
+	hobby = new char[len];
+	hobby[0] = '\0';
+	weight = 0;
 }
 
 
 Cow::Cow(const char* nm, const char* ho, double wt)
 {
 
+	strcpy_s(name, 21, nm);
+
+	int len = std::strlen(ho);
+	hobby = new char[len + 1];
+	strcpy_s(hobby, len + 1, ho);
+
+	weight = wt;
 }
 
 Cow::Cow(const Cow& c)
 {
+	strcpy_s(name, 21, c.name);
 
+	int len = std::strlen(c.hobby);
+	hobby = new char[len + 1];
+	strcpy_s(hobby, len + 1, c.hobby);
+
+	weight = c.weight;
 }
 
 Cow::~Cow()
 {
-
+	delete[] hobby;
 }
 
 Cow & Cow::operator=(const Cow& c)
 {
 
+	if (this == &c)
+		return *this;
+	delete name;
+	delete[] hobby;
+	
+	strcpy_s(name, 21, c.name);
+
+	int len = std::strlen(c.hobby);
+	hobby = new char[len + 1];
+	strcpy_s(hobby, len + 1, c.hobby);
+
+	weight = c.weight;
+
+	return *this;
+
+
 }
 
 void Cow::ShowCow() const
 {
-
+	cout << "Name " << name << std::endl;
+	cout << "Hobby: " << hobby << std::endl;
+	cout << "Weigh " << weight << std::endl;
 }
