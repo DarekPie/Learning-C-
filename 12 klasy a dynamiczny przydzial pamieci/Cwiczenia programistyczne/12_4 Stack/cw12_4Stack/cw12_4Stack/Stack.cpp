854be3fbd68d211a95e2cf227efede3f5629fb34
DarekDesk <darek.piechota@gmail.com>
2019-10-21 06:39:04 +0200

    Cw 12_4 Stack Utworzenie

---
+++ 12 klasy a dynamiczny przydzial pamieci/Cwiczenia programistyczne/12_4 Stack/cw12_4Stack/cw12_4Stack/Stack.cpp
@@ -0,0 +1,50 @@
+#include "stack.h"
+
+Stack::Stack(int n)
+{
+}
+
+Stack::Stack(const Stack & st)
+{
+}
+
+Stack::~Stack()
+{
+}
+
+bool Stack::isempty() const
+{
+	return top == 0;
+}
+
+bool Stack::isfull() const
+{
+	return top == MAX;
+}
+
+bool Stack::push(const Item & item)
+{
+	if (top < MAX)
+	{
+		pitems[top++] = item;
+		return true;
+	}
+	else
+		return false;
+}
+
+bool Stack::pop(Item & item)
+{
+	if (top > 0)
+	{
+		item = pitems[--top];
+		return true;
+	}
+	else
+		return false;
+}
+
+Stack & Stack::operator=(const Stack & st)
+{
+	// TODO: insert return statement here
+}
