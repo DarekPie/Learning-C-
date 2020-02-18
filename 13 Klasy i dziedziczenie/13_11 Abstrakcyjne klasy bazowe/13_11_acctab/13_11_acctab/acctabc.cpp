a20bfebd0a214800da90f7c9eca5f3c6a34e5488
DDW <darek.piechota@gmail.com>
2020-02-18 05:28:30 +0100

    13_11 AcctABC abstrakcyjne klasy bazowe

---
+++ 13 Klasy i dziedziczenie/13_11 Abstrakcyjne klasy bazowe/13_11_acctab/13_11_acctab/acctabc.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<cstring>
+#include "acctabc.h"
+using std::cout;
+using std::ios_base;
+using std::endl;
+
+AcctABC::AcctABC(const char *s, long an, double bal)
+{
+//	std::strncpy(fullName, s, MAX - 1);
+	strcpy_s(fullName, MAX - 1, s);
+	fullName[MAX - 1] = '\n';
+	acctNum = an;
+	balance = bal;
+}
+
+void AcctABC::Deposit(double amt)
+{
+	if (amt < 0)
+		cout << "Nie mozesz wyplacic ujemnej kwoty; Wplata anulowana.\n";
+	else
+		balance += amt;
+}
+
+void AcctABC::Withdraw(double amt)
+{
+	balance -= amt;
+}
+
+ios_base::fmtflags AcctABC::SetFormat() const
+{
+	//ustawia format ###.##
+	ios_base::fmtflags initialState = cout.setf(ios_base::fixed, ios_base::floatfield);
+	cout.setf(ios_base::showpoint);
+	cout.precision(2);
+	return initialState;
+}
+
+void Brass::Withdraw(double amt)
+{
+	if (amt < 0)
+		cout << "Nie mozna wyplacic ujemnej kwoty; Wyplata anulowana.\n";
+	else if (amt <= Balance())
+		AcctABC::Withdraw(amt);
+	else
+		cout << "Zadana wartosc " << amt << " zl przekracza dostepne srodki .\n" << "Wyplata anuloana.\n";
+}
+
+void Brass::ViewAcct() const
+{
+	ios_base::fmtflags initialState = SetFormat();
+	cout << "Wlasciciel archunku Brass: " << FullName() << endl;
+	cout << "Numer rachunku: " << AcctNum() << endl;
+	cout << "Stan konta: " << Balance() << " zl" << endl;
+	cout.setf(initialState);
+}
+
+//Metody klasy BrassPlus
+BrassPlus::BrassPlus(const char *s, long an, double bal, double ml, double r) : AcctABC(a, an, bal)
+{
+	maxLoan = ml;
+	owesBank = 0.0;
+	rate = r;
+}
+
+BrassPlus::BrassPlus(const Brass & ba, double ml, double r) : AcctABC(ba) // Uzywa niejawnego konstruktora kopiujacego
+{
+	maxLoan = ml;
+	owesBank = 0.0;
+	rate = r;
+}
+
+void BrassPlus::ViewAcct() const
+{
+	ios_base::fmtflags initialState = SetFormat();
+	cout << "Wlasciciel archunku Brass: " << FullName() << endl;
+	cout << "Numer rachunku: " << AcctNum() << endl;
+	cout << "Stan konta: " << Balance() << " zl" << endl;
+	cout << "Limit debetu: " << maxLoan << " zl " << endl;
+	cout << "Stopa oprocentowania: " << 100 * rate << "%\n";
+	cout.setf(initialState);
+}
+
+void BrassPlus::Withdraw(double amt)
+{
+	ios_base::fmtflags initialStete = SetFormat();
+	double bal = Balance();
+
+	if (amt <= bal)
+		AcctABC::Withdraw(amt);
+	else if (amt <= bal + maxLoan - owesBank)
+	{
+		double advance = amt - bal;
+		owesBank += advance * (1.0 + rate);
+		cout << "Zadluzenie faktyczne " << advance << " zl " << endl;
+		cout << "Odsetki: " << advance * rate << " zl " << endl;
+		AcctABC::Withdraw(amt);
+	}
+	else
+		cout << "Przekroczony limit debetu. Operacja anulowana.\n";
+	cout.setf(initialStete);
+}
+ 
---
+++ 13 Klasy i dziedziczenie/13_11 Abstrakcyjne klasy bazowe/13_11_acctab/13_11_acctab/acctabc.h
@@ -0,0 +1,54 @@
+#ifndef ACCTABC_H_
+#define ACCTABC_H_
+
+// Abstrakcyjna klasa bazowa
+class AcctABC
+{
+	private:
+		enum {MAX = 35};
+		char fullName[MAX];
+		long acctNum;
+		double balance;
+
+	protected:
+		const char * FullName() const { return fullName; }
+		long AcctNum() const { return acctNum; }
+		std::ios_base::fmtflags SetFormat() const;
+
+	public:
+		AcctABC(const char * s = "brak", long an = -1, double bal = 0.0);
+		void Deposit(double amt);
+		virtual void Withdraw(double amt) = 0;			// funkcja czysto wirtualna (bo = 0)
+		double Balance() const { return balance; }
+		virtual void ViewAcct() const = 0;				// Funkcja czysto wirtualna
+		virtual ~AcctABC() {}
+};
+
+class Brass : public AcctABC
+{
+	public:
+		Brass(const char * s = "brak", long an = -1, double bal = 0.0) : AcctABC(s, an, bal) {}
+		virtual void Withdraw(double amt);
+		virtual void ViewAcct() const;
+		virtual ~Brass() {}
+};
+
+class BrassPlus : public AcctABC
+{
+	private:
+		double maxLoan;
+		double rate;
+		double owesBank;
+		
+	public:
+		BrassPlus(const char *s = "brak", long an = -1, double bal = 0.0, double ml = 500, double r = 0.10);
+		BrassPlus(const Brass & ba, double ml = 500, double r = 0.1);
+		virtual void ViewAcct() const;
+		virtual void Withdraw(double amt);
+		void ResetMax(double m) { maxLoan = m; }
+		void ResetRate(double r) { rate = r; }
+		void ResetOwes() { owesBank = 0; }
+};
+#endif // !ACCTABC_H_
+
+
