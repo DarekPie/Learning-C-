69c9046b711fa5e1fcd9418bb0415856bc5ef458
DDW <darek.piechota@gmail.com>
2020-04-12 11:43:47 +0200

    cw 13_3 ABCdma klasa abstrakcyjna definicje skonczone

---
+++ 13 Klasy i dziedziczenie/Ćwiczenia programistyczne/13_3 Tablica Wskaznikow/cw13_3TabWskaz/cw13_3TabWskaz/dma.cpp
@@ -0,0 +1,158 @@
+#include "dma.h"
+#include <cstring>
+
+
+
+ABCdma::ABCdma(const char * s, int r)
+{
+	int lengh = std::strlen(s) + 1;
+	label = new char[lengh];
+	strcpy_s(label, lengh, s);
+	rating = r;
+}
+
+ABCdma::ABCdma(const ABCdma & rs)
+{
+	int lengh = std::strlen(rs.label) + 1;
+	label = new char[lengh];
+	strcpy_s(label, lengh, rs.label);
+	rating = rs.rating;
+}
+
+ABCdma & ABCdma::operator=(const ABCdma & rs)
+{
+	if (this == &rs)
+		return *this;
+
+	delete[] label;
+	label = new char[std::strlen(rs.label) + 1];		// zmien na security
+	rating = rs.rating;
+	return *this;
+}
+
+
+
+void baseDMA::View()
+{
+	std::cout << "Label = " << getLabel() << std::endl;
+	std::cout << "Rating = " << getRating() << std::endl;
+}
+
+baseDMA & baseDMA::operator=(const baseDMA & rs)
+{
+	if (this == &rs)
+		return *this;
+	ABCdma::operator=(rs);
+	return *this;
+}
+
+std::ostream & operator<<(std::ostream & os, const ABCdma & rs)
+{
+	os << "Etykieta: " << rs.label << std::endl;
+	os << "Klasa: " << rs.rating << std::endl;
+	return os;
+}
+
+std::ostream & operator<<(std::ostream & os, const baseDMA & rs)
+{
+	os << (const ABCdma&)rs;
+	return os;
+}
+
+std::ostream & operator<<(std::ostream & os, const lacksDMA & ls)
+{
+	os << (const ABCdma &)ls;
+	os << "Kolor: " << ls.color << std::endl;
+	return os;
+}
+
+std::ostream & operator<<(std::ostream & os, const hasDMA & hs)
+{
+	os << (const ABCdma &)hs;
+	os << "Styl: " << hs.style << std::endl;
+	return os;
+}
+
+
+
+lacksDMA::lacksDMA(const char * c, const char * l, int r) : ABCdma(l, r)
+{
+
+
+	//	std::strncpy(color, c, 39);			//!! zmien na security
+	strcpy_s(color, 39, c);
+	//	color[39] = 2 / 02;
+	color[39] = '\0';
+}
+
+lacksDMA::lacksDMA(const char * c, const ABCdma & rs) : ABCdma(rs)
+{
+	//	std::strncpy(color, c, COL_LEN - 1);			// zamien na securty!!!
+	strcpy_s(color, COL_LEN, c);			// COL_LEN czy COL_LEN - 1 ? 
+	color[COL_LEN - 1] = '\0';
+}
+
+void lacksDMA::View()
+{
+	std::cout << "Label = " << getLabel() << std::endl;
+	std::cout << "Rating = " << getRating() << std::endl;
+	std::cout << color << std::endl;
+
+}
+
+
+
+hasDMA::hasDMA(const char * s, const char * l, int r) : ABCdma(l, r)
+{
+	int lengh = std::strlen(s) + 1;
+	//	style = new char[std::strlen(s) + 1];
+	style = new char[lengh];
+	//	std::strcpy(style, s);
+	strcpy_s(style, lengh, s);
+}
+
+hasDMA::hasDMA(const char * s, const ABCdma & rs) : ABCdma(rs)
+{
+	int lengh = std::strlen(s) + 1;
+	//	style = new char[std::strlen(s) + 1];
+	style = new char[lengh];
+	//	std::strcpy(style, s);					//!!! zmien na security
+	strcpy_s(style, lengh, s);
+}
+
+hasDMA::hasDMA(const hasDMA & hs) :ABCdma(hs)		// wywoluje konstruktor kopiujacy klasy basowej
+{
+	int lengh = std::strlen(hs.style) + 1;
+	//	style = new char[std::strlen(hs.style) + 1];
+	style = new char[lengh];
+	//	std::strcpy(style, hs.style);
+	strcpy_s(style, lengh, hs.style);
+}
+
+hasDMA::~hasDMA()
+{
+	delete[] style;
+}
+
+void hasDMA::View()
+{
+	std::cout << "Label = " << getLabel() << std::endl;
+	std::cout << "Rating = " << getRating() << std::endl;
+	std::cout << "style = " << style << std::endl;
+}
+
+hasDMA & hasDMA::operator=(const hasDMA & hs)
+{
+	if (this == &hs)
+		return *this;
+
+	ABCdma::operator=(hs);			// Kopiuje dane klasy basowej
+	int lengh = std::strlen(hs.style) + 1;
+	//	style = new char[std::strlen(hs.style) + 1];
+	style = new char[lengh];
+	//	std::strcpy(style, hs.style);			// !! zmien na security
+	strcpy_s(style, lengh, hs.style);
+	return *this;
+}
+
+
---
+++ 13 Klasy i dziedziczenie/Ćwiczenia programistyczne/13_3 Tablica Wskaznikow/cw13_3TabWskaz/cw13_3TabWskaz/dma.h
@@ -0,0 +1,69 @@
+#ifndef DMA_H_
+#define DMA_H_
+#include <iostream>
+
+class ABCdma
+{
+private:
+	char  * label;
+	int rating;
+
+public:
+	ABCdma(const char * s = "brak", int r = 0);
+	ABCdma(const ABCdma & rs);
+	virtual ~ABCdma() {}
+	virtual void View() const = 0;					//Funkcja czysto writualna bo ma const = 0 i nie ma definicji
+	ABCdma & operator=(const ABCdma & rs);
+	const char * getLabel() const { return label; }
+	int getRating() const { return rating; }
+	friend std::ostream & operator << (std::ostream & os, const ABCdma & rs);
+};
+
+
+
+class baseDMA : public ABCdma
+{
+public:
+	baseDMA(const char * l = "brak", int r = 0) : ABCdma(l, r) {}
+	baseDMA(const baseDMA & rs) : ABCdma(rs) {}
+	virtual ~baseDMA() {}
+	virtual void View();
+	baseDMA & operator=(const baseDMA & rs);
+	friend std::ostream & operator<<(std::ostream & os, const baseDMA & rs);
+};
+
+
+//Niejawny konstruktor kopiujacy
+//Niejawny operator przypisania
+class lacksDMA : public ABCdma
+{
+private:
+	enum { COL_LEN = 40 };
+	char color[COL_LEN];
+
+public:
+	lacksDMA(const char *c = "brak", const char * l = "brak", int r = 0);
+	lacksDMA(const char *c, const ABCdma &rs);
+	virtual void View();
+	friend std::ostream & operator<<(std::ostream & os, const lacksDMA &rs);
+
+};
+
+// Klasa pochodna z dynamicznym przydzialem pamieci
+class hasDMA : public ABCdma
+{
+private:
+	char * style;
+
+public:
+	hasDMA(const char * s = "brak", const char * l = "brak", int r = 0);
+	hasDMA(const char * s, const ABCdma & rs);
+	hasDMA(const hasDMA & hs);				// wywoluje konstruktor kopiujacy klasy basowej
+	~hasDMA();
+	virtual void View();
+	hasDMA & operator=(const hasDMA & rs);
+	friend std::ostream & operator<<(std::ostream & os, const hasDMA & rs);
+};
+
+#endif
+
