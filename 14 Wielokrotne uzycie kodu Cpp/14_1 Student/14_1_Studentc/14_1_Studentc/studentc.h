5d2ac54d6b0b7a9b38df4a0d7a189a415606813d
DDW <darek.piechota@gmail.com>
2020-05-20 07:34:03 +0200

    14_1 Studentc

--- 14 Wielokrotne uzycie kodu Cpp/14_1 Student/14_1_Studentc/14_1_Studentc/studentc.h
+++ 14 Wielokrotne uzycie kodu Cpp/14_1 Student/14_1_Studentc/14_1_Studentc/studentc.h
@@ -12,13 +12,13 @@ class Student
 		std::string name;			// Obiekt zawierany
 		ArrayDb scores;				// Obiekt zawierany
 		//Prywatna metoda do wyswietlania wynikow
-		std::ostream & arr_out(std::osteam &os) const;
+		std::ostream & arr_out(std::ostream &os) const;
 	public:
 		Student()											: name("Brak"), scores() {}
 		Student(const std::string & s)						: name(s), scores() {}
-		explicit Student(int n)								: name("Brak"), scores(n) {}
+		explicit Student(int n)								: name("Brak"), scores(n) {}		// explicit powoduje wychwyceniu wyjatku zwiazanego z utworzeniem tablicy pustych obiektow
 		Student(const std::string & s, int n)				: name(s), scores(n) {}				// ? 
-		Student(const std::string & s, cont ArrayDb & a)	: name(s), scores(a) {}				// score - tablica wartosci typu ArrayDb wiec double
+		Student(const std::string & s, const ArrayDb & a)	: name(s), scores(a) {}				// score - tablica wartosci typu ArrayDb wiec double
 		Student(const char * str, const double * pd, int n)	: name(str), scores(pd, n) {}		//score - tablica n elementow zainicjalizowana 
 																								//za pomoc czterech pierwszych elementow tablicy pd
 		~Student() {}
