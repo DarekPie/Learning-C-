172f81b8c15a77eac5b28d551b62309424bc1f48
DDW <darek.piechota@gmail.com>
2020-05-26 07:35:03 +0200

    14_4_Studenti2

--- 14 Wielokrotne uzycie kodu Cpp/14_1 Student/14_1_Studentc/14_1_Studentc/studentc.h
+++ 14 Wielokrotne uzycie kodu Cpp/14_1 Student/14_1_Studentc/14_1_Studentc/studentc.h
@@ -19,9 +19,9 @@ class Student
 		explicit Student(int n)								: name("Brak"), scores(n) {}		// explicit powoduje wychwyceniu wyjatku zwiazanego z utworzeniem tablicy pustych obiektow
 		Student(const std::string & s, int n)				: name(s), scores(n) {}				// ? 
 		Student(const std::string & s, const ArrayDb & a)	: name(s), scores(a) {}				// score - tablica wartosci typu ArrayDb wiec double
-		Student(const char * str, const double * pd, int n)	: name(str), scores(pd, n) {}		//score - tablica n elementow zainicjalizowana 
-																								//za pomoc czterech pierwszych elementow tablicy pd
+		Student(const char * str, const double * pd, int n)	: name(str), scores(pd, n) {}		//score - tablica n elementow zainicjalizowana 																						//za pomoc czterech pierwszych elementow tablicy pd
 		~Student() {}
+
 		double Average() const;
 		const std::string & Name() const;
 		double &operator[](int i);
