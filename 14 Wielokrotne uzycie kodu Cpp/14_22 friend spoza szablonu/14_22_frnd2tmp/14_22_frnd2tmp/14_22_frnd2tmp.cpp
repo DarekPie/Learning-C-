8852c4bd757fb8521ca950497a2cab25f16cb1b5
DLenovo <darke.piechota@gmail.com>
2020-06-15 13:09:45 +0200

    14_22 frnd2tmp funkcje zaprzyjaznione spoza szablonu

---
+++ 14 Wielokrotne uzycie kodu Cpp/14_22 friend spoza szablonu/14_22_frnd2tmp/14_22_frnd2tmp/14_22_frnd2tmp.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+using std::cout;
+using std::endl;
+
+template <typename T>
+class HasFriend
+{
+	private:
+		T item;
+		static int ct;
+
+	public:
+		HasFriend(const T & i) : item(i) { ct++; }
+		~HasFriend() { ct--; }
+		friend void counts();
+		friend void reports(HasFriend<T> &);			//Parametr w postaci szablonu
+
+};
+
+//Kazda specjalizacja posiada wlasna statyczna dana skladowa
+template <typename T> 
+int HasFriend<T>::ct = 0;
+
+//Funkcja (nie szablon) zaprzyjazniona z wszystkimi klasami HasFriend<T>
+void counts()
+{
+	cout << "Konkretyzacje int: " << HasFriend<int>::ct << "; ";
+	cout << "Konkretyzacje double: " << HasFriend<double>::ct << endl;
+}
+
+//Funkcja (nie szablon) zaprzyjazniona z klasa HasFriend<int>
+void reports(HasFriend<int> & hf)
+{
+	cout << "HasFriend<int>: " << hf.item << endl;
+}
+
+//Funkcja (nie szablon) zaprzyjazniona z klasa HasFirend<double>
+void reports(HasFriend<double> & hf)
+{
+	cout << "HasFriend<double>: " << hf.item << endl;
+}
+
+int main()
+{
+	cout << "Brak zadeklarowanych obiektow: ";
+	counts();
+
+	HasFriend<int> hfi1(10);
+	cout << "Po deklaracji hfi1: ";
+	counts();
+
+	HasFriend<int> hfi2(20);
+	cout << "Po deklaracji hfi2: ";
+	counts();
+
+	HasFriend<double> hfdb(10.5);
+	cout << "Po deklaracji hfdb: ";
+	counts();
+
+	reports(hfi1);
+	reports(hfi2);
+	reports(hfdb);
+
+	return 0;
+
+}
