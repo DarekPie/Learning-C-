011f4406f82d4f2b6520096da017cf861b3860d5
DLenovo <darke.piechota@gmail.com>
2020-07-03 12:01:11 +0200

    cw 14_1_ poczatek

---
+++ 14 Wielokrotne uzycie kodu Cpp/ćwiczenia/cw_14_1_wine/cw_14_1_wine/cw_14_1_wine.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include "winec.h"
+int main()
+{
+	using std::cin;
+	using std::cout;
+	using std::endl;
+
+	cout << "Podaj nazwe wina: ";
+	char lab[50];
+	cin.getline(lab, 50);
+	cout << "Podaj liczbe rocznikow: ";
+	int yrs;
+	cin >> yrs;
+
+	cout << " Podaj nazwe: " << lab << " liczba rocznikow: " << yrs << endl;
+
+
+}
+
